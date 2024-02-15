9363cde3681bdbe919cb015d43c720f02bb8b1f4
xiao_wine <68009112+xiaowine@users.noreply.github.com>
2024-02-15 10:58:21 +0800

    Update CardPage.kt

--- app/src/main/java/cn/xiaowine/app/pages/CardPage.kt
+++ app/src/main/java/cn/xiaowine/app/pages/CardPage.kt
@@ -3,7 +3,6 @@ package cn.xiaowine.app.pages
 import android.annotation.SuppressLint
 import android.widget.Toast
 import cn.xiaowine.ui.WinePage
-
 @SuppressLint("SetTextI18n")
 class CardPage  : WinePage() {
 

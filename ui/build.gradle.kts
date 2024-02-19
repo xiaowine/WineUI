4ee70acaa175d054b404988cdc057de34e522f00
xiao_wine <xiaowine0@gmail.com>
2024-02-19 15:22:57 +0800

    0.0.3

--- ui/build.gradle.kts
+++ ui/build.gradle.kts
@@ -34,7 +34,7 @@ afterEvaluate {
             create<MavenPublication>("release") {
                 groupId = "cn.xiaowine.ui"
                 artifactId = "WineUI"
-                version = "0.0.2"
+                version = "0.0.3"
                 from(components["release"])
             }
         }
